6b650c90871ffa5edd829d409ce9072749259545
Heitor-OC <heitorocosta@gmail.com>
2026-04-24 13:09:20 -0300

    (controller): creating routes

--- src/main/java/com/example/cdn/controller/AssetController.java
+++ src/main/java/com/example/cdn/controller/AssetController.java
@@ -1,14 +1,33 @@
 package com.example.cdn.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cdn.service.AssetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/cdn")
+@RequiredArgsConstructor
 public class AssetController {
 
-    @GetMapping("/cdn/hi")
-    public String hi() {
-        return "Hello World";
+    private final AssetService assetService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
+        String filename = assetService.upload(file);
+        return ResponseEntity.ok("Arquivo enviado: " + filename);
     }
 
+    @GetMapping("/{filename}")
+    public ResponseEntity<Resource> download(@PathVariable String filename) {
+        Resource file = assetService.download(filename);
+
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                .body(file);
+    }
 }
