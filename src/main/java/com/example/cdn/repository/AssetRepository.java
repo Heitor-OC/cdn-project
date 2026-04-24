6ae21bc1e6d589d091bab79eccac776fadf915df
Heitor-OC <heitorocosta@gmail.com>
2026-04-24 12:42:45 -0300

    (service): added repository & upload service logic

---
+++ src/main/java/com/example/cdn/repository/AssetRepository.java
@@ -0,0 +1,12 @@
+package com.example.cdn.repository;
+
+import com.example.cdn.model.Asset;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AssetRepository extends JpaRepository<Asset, UUID> {
+    Optional<Asset> findByFilename(String filename);
+    boolean existsByFilename(String filename);
+}
---
+++ src/main/java/com/example/cdn/service/AssetService.java
@@ -0,0 +1,49 @@
+package com.example.cdn.service;
+
+import com.example.cdn.model.Asset;
+import com.example.cdn.repository.AssetRepository;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AssetService {
+
+        private final AssetRepository assetRepository;
+
+        @Value("${storage.path}")
+        private String storagePath;
+
+        public String upload(MultipartFile file) {
+
+            String filename = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+            Path path = Paths.get(storagePath).resolve(filename);
+
+            Files.createDirectories(path.getParent());
+            Files.write(path, file.getBytes());
+
+            Asset asset = Asset.builder()
+                    .filename(filename)
+                    .storagePath(path.toString())
+                    .contentType(file.getContentType())
+                    .size(file.getSize())
+                    .build();
+
+            assetRepository.save(asset);
+
+            log.info("O arquivo foi salvo com sucesso: ", filename);
+            return filename;
+        }
+}
